5724a7a30ce56979382c7ea07e29e08fcaf21254
Hsiao-Wei Wang <hwwang156@gmail.com>
2018-10-01 01:26:01 +0800

    Porting eth.beacon.helpers and eth.beacon.genesis_helpers

--- tests/beacon/helpers.py
+++ tests/beacon/helpers.py
@@ -1,8 +1,15 @@
 from eth.beacon.config import (
     DEFAULT_CONFIG,
 )
+
+from eth.beacon.types.block import (
+    Block,
+)
 from eth.beacon.types.validator_record import (
     ValidatorRecord,
+)
+from eth.constants import (
+    ZERO_HASH32,
 )
 
 
@@ -16,3 +23,18 @@ def mock_validator_record(pubkey, start_dynasty=0, config=DEFAULT_CONFIG):
         start_dynasty=start_dynasty,
         end_dynasty=config['default_end_dynasty']
     )
+
+
+def get_pseudo_chain(length):
+    """Get a pseudo chain, only slot_number and parent_hash are valid.
+    """
+    blocks = []
+    for slot in range(length * 3):
+        blocks.append(
+            Block(
+                slot_number=slot,
+                parent_hash=blocks[slot - 1].hash if slot > 0 else ZERO_HASH32
+            )
+        )
+
+    return blocks
